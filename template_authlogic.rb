39c72b6ab456d105a13c462b4ea6608bf9dd8b54
Fadhli Rahim <fadhlirahim@gmail.com>
2009-10-13 01:40:53 +1100

    Cucumber 0.4.0 can now accept home page & homepage path

--- template_authlogic.rb
+++ template_authlogic.rb
@@ -526,7 +526,7 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
     
-    when /the homepage/
+		when /the home\s?page/
       '/'
     when /the account page/
       '/account'
